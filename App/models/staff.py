9302095b929c8f2cd3acaeceef2e271c70a6734f
moniquebritto21 <moniquencbritto@gmail.com>
2023-10-24 00:50:58 +0000

    checking if its the model

--- App/models/staff.py
+++ App/models/staff.py
@@ -6,7 +6,7 @@ class Staff(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
-    review = db.relationship('Review', backref='user', lazy=True, cascade="all, delete-orphan")
+    review = db.relationship('Review', backref='staff', lazy=True, cascade="all, delete-orphan")
 
 
     def __init__(self, username, password):
